4a8424edc85c2df4bf67839ca72f0574d1a2cf62
David <thefeddy@gmail.com>
2026-02-18 15:33:57 -0500

    Fixing some smoothbrain coffeeless css, added 404 styles

--- app/root.tsx
+++ app/root.tsx
@@ -69,7 +69,7 @@ export function ErrorBoundary() {
         message = error.status === 404 ? "404" : "Error";
         details =
             error.status === 404
-                ? "The requested page could not be found."
+                ? "Excuse you, what are you doing here?"
                 : error.statusText || details;
     } else if (import.meta.env.DEV && error && error instanceof Error) {
         details = error.message;
@@ -77,14 +77,12 @@ export function ErrorBoundary() {
     }
 
     return (
-        <main className="pt-16 p-4 container mx-auto">
-            <h1>{message}</h1>
-            <p>{details}</p>
-            {stack && (
-                <pre className="w-full p-4 overflow-x-auto">
-                    <code>{stack}</code>
-                </pre>
-            )}
-        </main>
+        <>
+            <main id="four-oh-four">
+                <h1>{message}</h1>
+                <p>{details}</p>
+                <a href="/">Go back!</a>
+            </main>
+        </>
     );
 }
