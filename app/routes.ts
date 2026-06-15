3d7f750a187fd50cf17031304aa7fd2ae92cbdc1
David <thefeddy@gmail.com>
2026-06-15 18:35:43 -0400

    removed vanity, that may have broke it

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,5 @@ import { type RouteConfig, index, route, prefix } from "@react-router/dev/routes
 
 export default [
     index('routes/home.tsx'),
-    route("linkedin", "routes/handlers/external-redirect.ts", { id: "vanity-linkedin" }),
-    route("git", "routes/handlers/external-redirect.ts", { id: "vanity-git" })
+
 ] satisfies RouteConfig;
