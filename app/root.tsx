a8c52afe32443b17b41d3d7927c293e892566cc4
David <thefeddy@gmail.com>
2026-06-15 19:02:41 -0400

    removed unused fav icon meta

--- app/root.tsx
+++ app/root.tsx
@@ -44,9 +44,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
             <head>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/fav/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/fav/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/fav/favicon-16x16.png" />
                 <link rel="manifest" href="/fav/site.webmanifest" />
                 <Meta />
                 <Links />
